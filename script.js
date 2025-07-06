1f7d687029fb849dea9d4d670d07d761e4189f2f
seyeon-k-k <103121218+seyeon-k-k@users.noreply.github.com>
2025-07-06 07:56:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 
+const svg = d3.select("#vis")
+  .append("svg")
+  .attr("width", 500)
+  .attr("height", 300);
+
+svg.append("circle")
+  .attr("cx", 250)
+  .attr("cy", 150)
+  .attr("r", 80)
+  .attr("fill", "steelblue");
+
