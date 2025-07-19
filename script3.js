b89519acf76eb0dca42445e666812083b17994d5
seyeon-k-k <103121218+seyeon-k-k@users.noreply.github.com>
2025-07-18 19:39:44 -0500

    Update script3.js

--- script3.js
+++ script3.js
@@ -24,19 +24,56 @@ d3.csv("Scene3.csv").then(data => {
     d.year = +d.year;
   });
 
-    // 고유한 값 추출
-  const makes = [...new Set(data.map(d => d.make))].sort();
-  const models = [...new Set(data.map(d => d.model))].sort();
-  const years = [...new Set(data.map(d => d.year))].sort();
+// 전체 select 요소 선택
+const makeSelect = d3.select("#make-select");
+const yearSelect = d3.select("#year-select");
+const modelSelect = d3.select("#model-select");
 
-  // 드롭다운에 옵션 추가
-  const makeSelect = d3.select("#make-select");
-  const modelSelect = d3.select("#model-select");
-  const yearSelect = d3.select("#year-select");
+// make 드롭다운 채우기
+const makes = [...new Set(data.map(d => d.make))].sort();
+makes.forEach(make => makeSelect.append("option").text(make).attr("value", make));
 
-  makes.forEach(make => makeSelect.append("option").text(make).attr("value", make));
-  models.forEach(model => modelSelect.append("option").text(model).attr("value", model));
-  years.forEach(year => yearSelect.append("option").text(year).attr("value", year));
+// make 선택 시 year 옵션 업데이트
+makeSelect.on("change", function () {
+  const selectedMake = this.value;
+
+  // 해당 make에 해당하는 연도 추출
+  const filteredYears = [...new Set(
+    data.filter(d => d.make === selectedMake).map(d => d.year)
+  )].sort();
+
+  // 기존 year 옵션 초기화 후 재구성
+  yearSelect.selectAll("option").remove();
+  yearSelect.append("option").text("Select Year").attr("value", "");
+  filteredYears.forEach(year =>
+    yearSelect.append("option").text(year).attr("value", year)
+  );
+
+  // model 초기화
+  modelSelect.selectAll("option").remove();
+  modelSelect.append("option").text("Select Model").attr("value", "");
+});
+
+// year 선택 시 model 옵션 업데이트
+yearSelect.on("change", function () {
+  const selectedMake = makeSelect.property("value");
+  const selectedYear = +this.value;
+
+  // 해당 make + year에 해당하는 모델 추출
+  const filteredModels = [...new Set(
+    data
+      .filter(d => d.make === selectedMake && d.year === selectedYear)
+      .map(d => d.model)
+  )].sort();
+
+  // 기존 model 옵션 초기화 후 재구성
+  modelSelect.selectAll("option").remove();
+  modelSelect.append("option").text("Select Model").attr("value", "");
+  filteredModels.forEach(model =>
+    modelSelect.append("option").text(model).attr("value", model)
+  );
+});
+
 });
 
   
